06b6bba2de149db2b329673c4ffeaa7ba5bb6f09
Brenno Rodrigues <brennopereira6@gmail.com>
2024-02-20 04:36:47 -0300

    feat: adding initial data types and initializations

--- src/App.hs
+++ src/App.hs
@@ -4,21 +4,58 @@ module App
 
 import Web.Scotty (get, html, pathParam, scotty)
 import Options.Generic (getRecord)
+import Data.List (find)
+import Data.Time.Clock.POSIX (getPOSIXTime)
 
-nodes :: [Int]
-nodes = [4000, 4001, 4002]
+data Node = Node { nodeId :: Int, port :: Int }
+  deriving (Show)
+
+data Proposal = Proposal { proposalId :: Int, proposalValue :: Maybe String }
+  deriving (Show)
+
+data Proposer = Proposer { proposerId :: Int }
+  deriving (Show)
+
+data Acceptor = Acceptor { lastProposalId :: Int , acceptedProposal :: Maybe Proposal }
+  deriving (Show)
+
+getNodes :: Int -> (Maybe Node, [Node])
+getNodes selfId = (
+  find (\(Node { nodeId = nodeId' }) -> selfId == nodeId') nodes,
+  filter (\(Node { nodeId = nodeId' }) -> selfId /= nodeId') nodes
+  )
+    where
+      nodes = [
+        Node { nodeId = 0, port = 4000 },
+        Node { nodeId = 1, port = 4001 },
+        Node { nodeId = 2, port = 4002 }
+        ]
 
 getNodeId :: IO Int
 getNodeId = getRecord "Paxos-hs" :: IO Int
 
+createProposal :: Maybe String -> IO Proposal
+createProposal value = do
+  timestamp <- (round . (* 1000)) <$> getPOSIXTime
+  pure $ Proposal { proposalId = timestamp, proposalValue = value }
+
 app :: IO ()
 app = do
-  nodeId <- getNodeId
-  let port = nodes!!nodeId
+  selfId <- getNodeId
+  let (maybeSelfNode, nodes) = getNodes selfId
 
-  scotty port $ do
-    get "/:word" $ do
-      beam <- pathParam "word"
-      html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
-    get "/" $ do
-      html $ mconcat ["<h1>Hello world!</h1>"]
+  print (maybeSelfNode, nodes)
+
+  p <- createProposal $ Just "hello world"
+
+  print p
+
+  case maybeSelfNode of
+    Nothing -> pure ()
+    Just selfNode -> do
+      scotty (port selfNode) $ do
+        get "/:word" $ do
+          beam <- pathParam "word"
+          html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+        get "/" $ do
+          html $ mconcat ["<h1>Hello world!</h1>"]
