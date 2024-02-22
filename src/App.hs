ebce9b406375e45268deea6d4a00bc68dc29c635
Brenno Rodrigues <brennopereira6@gmail.com>
2024-02-22 01:09:47 -0300

    feat: adding full prepare phase sending and receiving messages

--- src/App.hs
+++ src/App.hs
@@ -5,13 +5,14 @@ module App
 import qualified Data.Text.Lazy as T
 import qualified Data.ByteString.Lazy.UTF8 as BLU
 
-import Web.Scotty (ActionM, jsonData, get, post, html, pathParam, scotty, liftIO)
-import Options.Generic (getRecord)
+import Web.Scotty (ActionM, jsonData, get, post, html, text, status, pathParam, scotty, liftIO)
+import Network.HTTP.Types.Status (notImplemented501, ok200, unauthorized401, conflict409)
 import Data.List (find)
 import Data.Time.Clock.POSIX (getPOSIXTime)
 import Data.Aeson (FromJSON, ToJSON, encode)
 import GHC.Generics (Generic)
-import Network.HTTP (postRequestWithBody, rspBody, simpleHTTP)
+import Network.HTTP (postRequestWithBody, rspBody, rspCode, simpleHTTP)
+import Network.HTTP.Base (ResponseCode)
 import Control.Concurrent.STM.TVar (TVar, newTVarIO, readTVar, modifyTVar')
 import GHC.Conc (atomically)
 
@@ -38,9 +39,14 @@ data Acceptor = Acceptor {
   }
   deriving (Show)
 
+data Learner = Learner {
+  commitedProposal :: Maybe Proposal
+  }
+  deriving (Show)
+
 initialProposer :: Proposer
 initialProposer = Proposer {
-  proposerId = 10
+  proposerId = 0 -- TODO: implement monotonic accumulator
   }
 
 initialAcceptor :: Acceptor
@@ -52,97 +58,150 @@ initialAcceptor = Acceptor {
 initialnodes :: [Node]
 initialnodes = [
   Node {
-    nodeId = 0,
+    nodeId = 1,
     port = 4000,
     proposer = Just initialProposer,
     acceptor = Nothing
     },
   Node {
-    nodeId = 1,
+    nodeId = 2,
     port = 4001,
     proposer = Nothing,
     acceptor = Just initialAcceptor
     },
   Node {
-    nodeId = 2,
+    nodeId = 3,
     port = 4002,
+    proposer = Just initialProposer,
+    acceptor = Just initialAcceptor
+    },
+  Node {
+    nodeId = 4,
+    port = 4003,
+    proposer = Just initialProposer,
+    acceptor = Just initialAcceptor
+    },
+  Node {
+    nodeId = 5,
+    port = 4004,
     proposer = Just initialProposer,
     acceptor = Just initialAcceptor
     }
   ]
 
-getNodeId :: IO Int
-getNodeId = getRecord "Paxos-hs" :: IO Int
-
 getNodes :: Int -> (Maybe Node, [Node])
 getNodes selfId = (
-  find (\(Node { nodeId = nodeId' }) -> selfId == nodeId') initialnodes,
-  filter (\(Node { nodeId = nodeId' }) -> selfId /= nodeId') initialnodes
+  find (
+    \(Node { nodeId = nodeId' }) -> selfId == nodeId'
+    ) initialnodes,
+  filter (
+    \(Node { nodeId = nodeId' }) -> selfId /= nodeId'
+    ) initialnodes
   )
 
 updateAcceptorLastId :: Int -> Node -> Node
-updateAcceptorLastId newId node'@(Node { acceptor=(Just acceptor) }) =
+updateAcceptorLastId newId node'@(Node { acceptor=(Just acceptor') }) =
   node' {
-    acceptor = Just $ acceptor {
+    acceptor = Just $ acceptor' {
       lastProposalId = newId
       }
     }
 updateAcceptorLastId _ n = n
 
-{- 
+updateAcceptorProposal :: Maybe Proposal -> Node -> Node
+updateAcceptorProposal newProposal node'@(Node { acceptor=(Just acceptor') }) =
+  node' {
+    acceptor = Just $ acceptor' {
+      acceptedProposal = newProposal
+      }
+    }
+updateAcceptorProposal _ n = n
+
 createProposal :: Maybe String -> IO Proposal
 createProposal value = do
   timestamp <- (round . (* 1000)) <$> getPOSIXTime
   pure $ Proposal { proposalId = timestamp, proposalValue = value }
- -}
 
-setupPaxos :: IO (Either String (Node, [Node]))
-setupPaxos = do
-  selfId <- getNodeId
+setupPaxos :: Int -> IO (Either String (Node, [Node]))
+setupPaxos selfId = do
   case getNodes selfId of
     (Nothing, _) -> pure $ Left "Invalid id"
     (Just self, nodes) -> pure $ Right (self, nodes)
 
-{- sendProposal :: Proposal -> [Node] -> IO ()
-sendProposal proposal _ = do
-  let req = postRequestWithBody "http://0.0.0.0:4000" "application/json" $ BLU.toString $ encode proposal
-  res <- simpleHTTP req
-  case res of
-    Right a -> print $ rspBody a
-    Left err -> error $ show err -}
+sendProposal :: Proposal -> Node -> IO (Either String ResponseCode)
+sendProposal proposal node@(Node { port = port' }) = do
+  print $ "sending: " <> show proposal
+  print $ "to: " <> show node
+  let req = postRequestWithBody ("http://0.0.0.0:" <> show port' <> "/acceptor/prepare") "application/json" $ BLU.toString $ encode proposal
+  response <- simpleHTTP req
+  case response of
+    Right res -> pure $ Right $ rspCode res
+    Left err -> pure $ Left $ show err
 
-getProposal :: TVar Node -> [Node] -> ActionM ()
-getProposal stateTM _ = do
+postProposal :: TVar Node -> [Node] -> ActionM ()
+postProposal stateTM nodes = do
+  value :: Maybe String <- jsonData
+  proposal <- liftIO $ createProposal value
   self <- liftIO . atomically $ readTVar stateTM
   case self of
     Node { proposer=(Just proposer') } -> do
-      html $ "<h1>" <> (T.pack $ show $ proposerId proposer') <> "</h1>"
-    _ -> html "<h1>not a proposer</h1>"
+      liftIO . print $ "proposer: " <> show proposer'
+      responses <- liftIO $ sequence $ map (sendProposal proposal) nodes
+      liftIO . print $ "responses: " <> show responses
+      let majority = 1 + ((length nodes) `div` 2)
+      let successes = length $ filter (== Right (2, 0, 0)) responses
+      if majority > successes
+        then status conflict409 
+        else status ok200
+    _ -> status unauthorized401
 
 postPrepare :: TVar Node -> ActionM ()
 postPrepare stateTM = do
   proposal :: Proposal <- jsonData
-  _ <- liftIO $ print proposal
+  liftIO . print $ "prepare received: " <> show proposal
   self' <- liftIO . atomically $ readTVar stateTM
-  _ <- liftIO $ print self'
+  liftIO . print $ "prepare state: " <> show self'
   case self' of
     Node { acceptor=(Just acceptor') } -> do
       if lastProposalId acceptor' >= proposalId proposal
-        then html $ "<h1>already received a higher id</h1>"
+        then status conflict409
         else do
           liftIO . atomically $ modifyTVar' stateTM $ updateAcceptorLastId $ proposalId proposal
           self <- liftIO . atomically $ readTVar stateTM
-          _ <- liftIO $ print self
-          html $ "<h1>" <> (T.pack $ show $ lastProposalId acceptor') <> "</h1>"
-    _ -> html "<h1>not a acceptor</h1>"
+          _ <- liftIO $ print $ "prepare state: " <> show self
+          -- TODO: drop timestamp and use a distributed monotonic accumulator
+          -- text $ T.pack $ show $ lastProposalId acceptor'
+          status ok200
+    _ -> status unauthorized401
 
-app :: IO ()
-app = do
-  paxos <- setupPaxos
+postCommit :: TVar Node -> ActionM ()
+postCommit stateTM = do
+  proposal :: Proposal <- jsonData
+  liftIO $ print $ "commit received: " <> show proposal
+  self' <- liftIO . atomically $ readTVar stateTM
+  liftIO $ print $ "commit state: " <> show self'
+  case self' of
+    Node { acceptor=(Just acceptor') } -> do
+      if lastProposalId acceptor' >= proposalId proposal
+        then status conflict409
+        else do
+          liftIO . atomically $ modifyTVar' stateTM $ updateAcceptorLastId $ proposalId proposal
+          self <- liftIO . atomically $ readTVar stateTM
+          _ <- liftIO $ print $ "commit state: " <> show self
+          -- TODO: drop timestamp and use a distributed monotonic accumulator
+          -- text $ T.pack $ show $ lastProposalId acceptor'
+          status ok200
+    _ -> status unauthorized401
+
+app :: Int -> IO ()
+app selfId = do
+  paxos <- setupPaxos selfId
   case paxos of
     Right (self, nodes) -> do
       state <- newTVarIO self :: IO (TVar Node)
       scotty (port self) $ do
-        get "/proposer" $ getProposal state nodes
+        post "/proposer" $ postProposal state nodes
         post "/acceptor/prepare" $ postPrepare state
+        post "/acceptor/commit" $ postCommit state
+        post "/" $ html "<marquee>Hello world</marquee>"
     Left err -> print err
